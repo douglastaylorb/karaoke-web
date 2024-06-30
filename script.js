e91a49c3364b3f17c75433eecf44343f35912f8a
Douglas Taylor <douglastaylor20@gmail.com>
2024-06-30 08:51:39 -0300

    adiciona nota

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+let modal = document.querySelector('.modal-content')
+
 document.querySelector('#nomeVideo').addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         buscarVideo()
     }
 })
-
 function buscarVideo() {
     const nomeVideo = document.querySelector('#nomeVideo').value
     const video = document.querySelector('#meuVideo')
@@ -29,8 +30,40 @@ function buscarVideo() {
             video.requestFullscreen()
     }
     
+    function gerarNota() {
+        const min = 85;
+        const max = 99;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+
     video.onended = function() {
         document.exitFullscreen()
-        window.location.href = 'index.html'
+        
+  
+        let nota = gerarNota()
+        
+        document.querySelector('.modal-content').style.display = 'flex'
+
+        if (nota >= 85 || nota <= 90) {    
+            modal.innerHTML = `<div class="modal-content--titulo"><h2>Muito Bem!</h2></div>
+            <div class="modal-content--texto">Você tirou</div>
+            <div class="modal-content--pontos"><${nota}></div>
+            <div class="modal-content--texto"><p>pontos!</p></div>`
+        } else if (nota >= 91 || nota <= 95) {
+            modal.innerHTML = `<div class="modal-content--titulo"><h2>Arrasou!</h2></div>
+            <div class="modal-content--texto">Você tirou</div>
+            <div class="modal-content--pontos"><${nota}></div>
+            <div class="modal-content--texto"><p>pontos!</p></div>`
+        } else if (nota >= 96 || nota <= 99) {
+            modal.innerHTML = `<div class="modal-content--titulo"><h2>Você é um(a) Cantor(a) Profissional!</h2></div>
+            <div class="modal-content--texto">Você tirou</div>
+            <div class="modal-content--pontos"><${nota}></div>
+            <div class="modal-content--texto"><p>pontos!</p></div>`
+        }
+
+        setTimeout(() => {
+            document.querySelector('.modal-content').style.display = 'none'
+            window.location.href = 'index.html'
+        }, 5000)
     }
 }
